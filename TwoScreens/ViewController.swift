0b544462932b1d8e1843e8df9fa78f1644bfaaa6
Aleksey Zarudny <zarudnyy2@gmail.com>
2022-03-08 16:05:22 +0300

    Initial Commit

---
+++ TwoScreens/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwoScreens
+//
+//  Created by Алексей Зарудный on 08.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
