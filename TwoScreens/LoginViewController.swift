6d53d81d6da288127732dbed8fde5220afc5d616
Aleksey Zarudny <zarudnyy2@gmail.com>
2022-03-09 19:32:34 +0300

    setup logic

--- TwoScreens/LoginViewController.swift
+++ TwoScreens/LoginViewController.swift
@@ -12,16 +12,13 @@ class LoginViewController: UIViewController {
     @IBOutlet var userNameTextField: UITextField!
     @IBOutlet var passwordTextField: UITextField!
     
-    private let user = "user"
-    private let password = "12345"
+    private let user = "qwe"
+    private let password = "qwe"
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
-    
-//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        super .touchesBegan(touches, with: event)
-//    }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let welcomVC = segue.destination as? WelcomeViewController else { return }
@@ -29,15 +26,25 @@ class LoginViewController: UIViewController {
     }
     
     @IBAction func logInButton(_ sender: UIButton) {
+        if userNameTextField.text != user || passwordTextField.text != password {
+            showAlert(title: "ERROR", message: "Please enter a valid username or password")
+        }
     }
     @IBAction func forgotUserNameButton() {
+        showAlert(title: "Username is", message: user)
     }
     @IBAction func forgotPasswordButton() {
+        showAlert(title: "Password is", message: password)
         
     }
     @IBAction func unwind(for segue: UIStoryboardSegue) {
         userNameTextField.text = ""
         passwordTextField.text = ""
-        }
+    }
+    private func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
 }
-
